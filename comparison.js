9b26f42b5381514c68870748971b4ed5a4d5c965
developerabdulaziz <abdulaziz01716114719@gmail.com>
2022-10-05 20:52:08 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,67 @@
+
+/*
+<   এইটা দিয়ে চেক করবে বাম পাশে যেটা আছে সেটা ডানপাশেরটা থেকে ছোট কিনা 
+
+>  এইটা দিয়ে চেক করবে বাম পাশে যেটা আছে সেটা ডানপাশেরটা থেকে বড় কিনা 
+
+==  এইটা দিয়ে চেক করবে বাম পাশেরটা আর ডানপাশেরটা দুইটাই সমান কিনা
+
+!=  এইটা দিয়ে চেক করবে বাম পাশেরটা আর ডানপাশেরটা দুইটাই অসমান (ডিফারেন্ট বা আলাদা) কিনা
+
+<=  এইটা দিয়ে চেক করবে বাম পাশে যেটা আছে সেটা ডানপাশেরটা থেকে ছোট বা সমান কিনা 
+
+>=  এইটা দিয়ে চেক করবে বাম পাশে যেটা আছে সেটা ডানপাশেরটা থেকে বড় বা সমান কিনা 
+ 
+এই ছয়টা জিনিস বুঝতে পারলে তোমার কন্ডিশন এর অলমোস্ট অর্ধেক কাজ শেষ। এর পর আসবে আরো দুইটা স্পেশাল জিনিস */
+
+
+
+//  COMPARISON OPERATORS 
+// >  Greater Than
+// <  Less Than 
+// >= Greater Than or Equal to
+// <= Less Than or Equal to
+// == Equal to
+// != Not Equal to
+
+
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+
+
+// less than or equal
+// console.log(5 <= 6);
+// console.log(16 <= 6);
+
+
+// greater than or equal
+// console.log(5 >= 6);
+// console.log(5 >= 2);
+// console.log(2 >= 2);
+
+
+var num1 = 56;
+var num2 = 89;
+console.log(num1);
+
+
+
+var myLove = 99;
+var yourLove = 100;
+// console.log(myLove > yourLove);
+// console.log(myLove < yourLove);
+// console.log(myLove == yourLove);
+// console.log(myLove < yourLove);
+// console.log(myLove != yourLove);
+
+
+//multiple conditions
+// money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 || result1 > result2
+
+//sorto puroner khetre &&-and ||-or use hoi
+// console.log(studyGood && goodperson);
+// console.log(isBanker || goodResult);
