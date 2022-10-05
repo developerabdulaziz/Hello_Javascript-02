bf089d0e7c3103175f09f6bbeb6e87c631a1d635
developerabdulaziz <abdulaziz01716114719@gmail.com>
2022-10-05 21:29:39 +0600

    conditional.js

---
+++ conditional.js
@@ -0,0 +1,39 @@
+var isFoodReady = true;
+// isFoodReady = false;
+// jodi food ready hoi taile ami khamu
+// if food is ready i will eat
+// if food is ready == true
+// if(condition) {
+
+// }
+
+// if (isFoodReady == true) {
+//     console.log('Mama!! I will eat now.');
+// }
+
+var iphonePrice = 70000;
+var myBudget = 109500;
+// if iphone price is less than by budget. I will buy the iphone
+
+// if (iphonePrice < myBudget) {
+//     console.log('iPhone diye futani marbo');
+// }
+// if (iphonePrice > myBudget) {
+//     console.log('Nokia diye futani marbo');
+// }
+
+
+var chickenPrice = 180;
+var myMoney = 850;
+/*
+// if chicken or price is less than my budget
+// i will eat chicken
+// na hoi: else
+*/
+
+if (chickenPrice < myMoney) {
+    console.log('yes ! murgir run khamu r haddi chabamu');
+}
+else {
+    console.log('No ! birani khamu');
+}
