587536e09ccc713d36a869ed6e0f25ba44c11296
developerabdulaziz <abdulaziz01716114719@gmail.com>
2022-10-06 15:38:39 +0600

    nested-condition

---
+++ multi-stage-condition.js
@@ -0,0 +1,58 @@
+
+// step ---------------------01
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+    console.log('dan dan danish khamu');
+}
+else if (butterBread < money) {
+    console.log('Butter bon khamu');
+}
+else if (toastBiscuit < money) {
+    console.log('chubai chubai toast biscuit khamu');
+}
+else {
+    console.log('khali cha e sasther jonno valo');
+}
+
+
+
+
+
+
+// step---------------------02
+
+// var danishPrice = 50;
+// var butterBreadPrice = 25;
+// var toastBiscuitPrice = 10;
+// var myBudget = 100;
+// paclWell = false;
+
+
+// if (danishPrice < myBudget) {
+//     console.log('Danish kheye danish jabo');
+// }
+// else if (butterBreadPrice < myBudget) {
+//     console.log('Butter bon diye cha khamu');
+// }
+// else if (toastBiscuitPrice < myBudget) {
+//     console.log('toast biscuit khamu')
+// }
+// else {
+//     console.log('Batas diye cha khamu');
+// }
+
+
+/*
+if (danishPrice < myBudget) {
+    if (paclWell == true) {
+        console.log('danish khabo');
+    }
+    else {
+        console.log('danish khamu na')
+    }
+}
+*/
---
+++ nested-condition.js
@@ -0,0 +1,21 @@
+
+var math = true;
+var geometry = true;
+var straightLine = false;
+
+if (math == true) {
+    if (geometry == true) {
+        if (straightLine == true) {
+
+        }
+        else {
+            console.log('baka pothe cholbana');
+        }
+    }
+    else {
+        console.log('pithgoras hoite parbana');
+    }
+}
+else {
+
+}
