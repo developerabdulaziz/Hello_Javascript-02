fe53fd62bd7b1dc8e3155ee57542e550aa4cd05a
developerabdulaziz <abdulaziz01716114719@gmail.com>
2022-10-07 11:17:12 +0600

    forloop

---
+++ for-loop.js
@@ -0,0 +1,35 @@
+// ===================For Loop=================
+
+/* var rostGiven = 0;
+while (rostGiven < 7) {
+    console.log('rost dan plz');
+    console.log(rostGiven);
+    rostGiven++
+}
+ */
+
+/* for (rostGiven = 0; rostGiven < 7; rostGiven++) {
+    console.log("rost dan plz");
+    console.log(rostGiven);
+    rostGiven = rostGiven + 1;
+}
+ */
+
+// simple version of for loop
+/* for (i = 0; i < 7; i++) {
+    console.log(i);
+} */
+
+// i = i + 2;
+//i += 2;
+
+/* // even nmbr
+for (i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+ */
+
+/* // Odd nmbr
+for (i = 1; i <= 10; i += 2) {
+    console.log(i);
+} */
