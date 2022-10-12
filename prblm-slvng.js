ecc5891a01405db11d56074ca9a642120ec07d0f
developerabdulaziz <abdulaziz01716114719@gmail.com>
2022-10-12 22:57:21 +0600

    prolem solbing 01

--- prblm-slvng.js
+++ prblm-slvng.js
@@ -41,6 +41,8 @@ You are given three numbers 13, 79 and 45. Write a program that will print the l
 You are given a triangle with the sides 9,8,9. Write a program to check whether a triangle is Isosceles or not using if-else.
 Isosceles => two sides are equal
  */
+
+/*
 var num1 = 13;
 var num2 = 79;
 var num3 = 45;
@@ -69,6 +71,8 @@ var side3 = 9;
 if (side1 == side2 || side1 == side3 || side2 == side3) {
     console.log('Isosceles');
 }
+*/
+
 
 // ========================================= Problem - 04============================================
 
